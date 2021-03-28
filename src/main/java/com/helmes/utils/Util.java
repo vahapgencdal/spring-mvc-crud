5675786a4171ef3a3778ada7427453bc08060936
vahapgencdal <avahap19@gmail.com>
2021-03-29 01:36:55 +0300

    userController tests added

--- src/main/java/com/helmes/utils/Util.java
+++ src/main/java/com/helmes/utils/Util.java
@@ -1,8 +1,14 @@
 package com.helmes.utils;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.helmes.dto.SectorFormDto;
 import com.helmes.model.Sector;
+import org.springframework.http.MediaType;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +22,10 @@ public class Util {
 
     public static final String SPACE = "&nbsp;&nbsp;&nbsp;&nbsp;";
 
+    public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            StandardCharsets.UTF_8
+    );
 
 
     public static String leftPad(final String str, final int size) {
@@ -42,5 +52,21 @@ public class Util {
             sortedList.add(sectorFormDto);
             mapSectorToForm(new ArrayList<>(sector.getSectors()), sortedList);
         });
+    }
+
+    public static byte[] convertObjectToJsonBytes(Object object) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        return mapper.writeValueAsBytes(object);
+    }
+
+    public static String createStringWithLength(int length) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int index = 0; index < length; index++) {
+            builder.append("a");
+        }
+
+        return builder.toString();
     }
 }
--- src/test/java/com/helmes/UsersControllerTest.java
+++ src/test/java/com/helmes/UsersControllerTest.java
@@ -1,7 +1,11 @@
 package com.helmes;
 
 
+import com.helmes.dto.UserFormDto;
+import com.helmes.dto.UserListDto;
+import com.helmes.model.User;
 import com.helmes.service.UserService;
+import com.helmes.utils.Util;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,7 +13,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 /**
  * @author Vahap Gencdal
@@ -29,16 +48,70 @@ class UsersControllerTest {
 
     @Test
     void user_list_test() throws Exception {
-        Assertions.assertTrue(Boolean.TRUE);
+        List<UserListDto> expectedList = new ArrayList<>();
+        UserListDto userListDto = new UserListDto();
+        userListDto.setId(1L);
+        userListDto.setName("test1");
+        userListDto.setAgreeToTerm(Boolean.TRUE);
+
+        UserListDto userListDto2 = new UserListDto();
+        userListDto2.setId(2L);
+        userListDto2.setName("test2");
+        userListDto2.setAgreeToTerm(Boolean.TRUE);
+        expectedList.add(userListDto);
+        expectedList.add(userListDto2);
+
+        when(userService.findAll()).thenReturn(expectedList);
+
+
+        MockHttpServletResponse mvcResponse = mockMvc
+                .perform(MockMvcRequestBuilders.get("/"))
+                .andReturn().getResponse();
+
+        assertTrue(mvcResponse.getContentAsString().contains("test1"));
+        assertEquals("text/html;charset=UTF-8",mvcResponse.getContentType());
+        assertEquals(200, mvcResponse.getStatus());
     }
 
     @Test
     void user_edit_test() throws Exception {
-        Assertions.assertTrue(Boolean.TRUE);
+        UserFormDto userFormDto = new UserFormDto();
+        userFormDto.setSectorIdList(Arrays.asList(1L,2L));
+        userFormDto.setAgreeToTerm(Boolean.TRUE);
+        userFormDto.setName("test1");
+
+
+        when(userService.getUserDto(any())).thenReturn(userFormDto);
+
+        MockHttpServletResponse mvcResponse = mockMvc
+                .perform(MockMvcRequestBuilders.get("/edit/1"))
+                .andReturn().getResponse();
+
+        assertTrue(mvcResponse.getContentAsString().contains("test1"));
+        assertEquals("text/html;charset=UTF-8",mvcResponse.getContentType());
+        assertEquals(200, mvcResponse.getStatus());
+
+
     }
 
     @Test
     void user_post_test() throws Exception {
-        Assertions.assertTrue(Boolean.TRUE);
+        UserFormDto userFormDto = new UserFormDto();
+        userFormDto.setSectorIdList(Arrays.asList(1L,2L));
+        userFormDto.setAgreeToTerm(Boolean.TRUE);
+        userFormDto.setName("test");
+
+        User user = new User();
+        user.setId(1L);
+        user.setName("test");
+
+        when(userService.save(any())).thenReturn(user);
+        MvcResult mvcResult = mockMvc.perform(post("/add")
+                .contentType(Util.APPLICATION_JSON_UTF8)
+                .content(Util.convertObjectToJsonBytes(userFormDto))
+        ).andReturn();
+
+        assertEquals(200, mvcResult.getResponse().getStatus());
+
     }
 }
